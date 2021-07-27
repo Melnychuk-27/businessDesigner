ac0f9bbd7cd97b49c31b50cb953391ddc4d38f8e
Vasja <qwertyqaz181207@gmail.com>
2021-07-27 20:03:16 +0300

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1.45,
+        spaceBetween: 20,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
